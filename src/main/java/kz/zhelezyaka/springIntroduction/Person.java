a856d66fc61bb4b74c03abd9e70badd2910470ac
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2022-03-30 06:48:59 +0600

    Dependency Injection with setter

--- src/main/java/kz/zhelezyaka/springIntroduction/Person.java
+++ src/main/java/kz/zhelezyaka/springIntroduction/Person.java
@@ -3,8 +3,12 @@ package kz.zhelezyaka.springIntroduction;
 public class Person {
     private Pet pet;
 
-    public Person(Pet pet) {
+    public Person() {
         System.out.println("Person bean is created");
+    }
+
+    public void setPet(Pet pet) {
+        System.out.println("Class Person: we're set up pet");
         this.pet = pet;
     }
 
