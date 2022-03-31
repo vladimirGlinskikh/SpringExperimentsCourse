26c809b76ae7fe6f7eb6f45f4d77710827f1d923
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2022-03-31 23:30:18 +0600

    Dependency Injection with String and other values

--- src/main/java/kz/zhelezyaka/springIntroduction/Person.java
+++ src/main/java/kz/zhelezyaka/springIntroduction/Person.java
@@ -2,6 +2,26 @@ package kz.zhelezyaka.springIntroduction;
 
 public class Person {
     private Pet pet;
+    private String surname;
+    private int age;
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        System.out.println("Class Person: set surname");
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        System.out.println("Class Person: set age");
+        this.age = age;
+    }
 
     public Person() {
         System.out.println("Person bean is created");
--- src/main/java/kz/zhelezyaka/springIntroduction/Test3.java
+++ src/main/java/kz/zhelezyaka/springIntroduction/Test3.java
@@ -8,6 +8,8 @@ public class Test3 {
                 new ClassPathXmlApplicationContext("applicationContext.xml");
         Person person = context.getBean("myPerson", Person.class);
         person.callYourPet();
+        System.out.println(person.getSurname());
+        System.out.println(person.getAge());
         context.close();
     }
 }
