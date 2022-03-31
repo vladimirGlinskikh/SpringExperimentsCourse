ed48b547a5387454a15a09cdec390f90c6fb0e62
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2022-04-01 01:40:09 +0600

    Bean scope

--- src/main/java/kz/zhelezyaka/springIntroduction/Dog.java
+++ src/main/java/kz/zhelezyaka/springIntroduction/Dog.java
@@ -1,6 +1,15 @@
 package kz.zhelezyaka.springIntroduction;
 
 public class Dog implements Pet {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public Dog() {
         System.out.println("Dog bean is created");
