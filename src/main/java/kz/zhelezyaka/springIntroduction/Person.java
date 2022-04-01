79d442dd6d5c95bc5d96ed339d0846b4d5311480
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2022-04-01 07:01:29 +0600

    Annotation Autowired for setter

--- src/main/java/kz/zhelezyaka/springIntroduction/Person.java
+++ src/main/java/kz/zhelezyaka/springIntroduction/Person.java
@@ -31,11 +31,18 @@ public class Person {
 //        System.out.println("Person bean is created");
 //    }
 
+
     @Autowired
-    public void setPet(Pet pet) {
-        System.out.println("Class Person: we're set up pet");
+    public Person(Pet pet) {
+        System.out.println("Person bean is created");
         this.pet = pet;
     }
+
+//    @Autowired
+//    public void setPet(Pet pet) {
+//        System.out.println("Class Person: we're set up pet");
+//        this.pet = pet;
+//    }
 
     public void callYourPet() {
         System.out.println("Hello, my pet!");
