dfbc7a18659346c9b23ab5952afb72eec05798f4
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2022-04-16 14:02:55 +0600

    Created database my_db and created table employees

---
+++ resources/01.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS my_db;
+CREATE DATABASE IF NOT EXISTS my_db;
+USE my_db;
+
+CREATE TABLE employees
+(
+    id         int NOT NULL AUTO_INCREMENT,
+    name       VARCHAR(15),
+    surname    VARCHAR(25),
+    department VARCHAR(20),
+    salary     int,
+    PRIMARY KEY (id)
+);
