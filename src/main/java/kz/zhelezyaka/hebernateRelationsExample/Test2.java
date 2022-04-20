e31cbf7a28e8636d3b4dcca0fb02c852fa4ce12b
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2022-04-20 20:15:26 +0600

    Relationship One-to-One (Bi-directional)

---
+++ src/main/java/kz/zhelezyaka/hebernateRelationsExample/Test2.java
@@ -0,0 +1,61 @@
+package kz.zhelezyaka.hebernateRelationsExample;
+
+import kz.zhelezyaka.hebernateRelationsExample.entity.Detail;
+import kz.zhelezyaka.hebernateRelationsExample.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Test2 {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .addAnnotatedClass(Detail.class)
+                .buildSessionFactory();
+
+        Session session = null;
+        try {
+//            session = factory.getCurrentSession();
+//            Employee employee = new Employee(
+//                    "Olesya",
+//                    "Shvec",
+//                    "HR", 750);
+//
+//            Detail detail = new Detail(
+//                    "Almaty",
+//                    "+77017895670",
+//                    "springOlesya@gmail.com");
+//
+//            employee.setEmployeeDetail(detail);
+//            detail.setEmployee(employee);
+//
+//            session.beginTransaction();
+//            session.save(detail);
+//            session.getTransaction().commit();
+//
+//            System.out.println("Transaction was well done");
+
+//            session = factory.getCurrentSession();
+//
+//            session.beginTransaction();
+//            Detail detail = session.get(Detail.class, 5);
+//            System.out.println(detail.getEmployee());
+//            session.getTransaction().commit();
+//
+//            System.out.println("Transaction was well done");
+
+            session = factory.getCurrentSession();
+
+            session.beginTransaction();
+            Detail detail = session.get(Detail.class, 5);
+            session.delete(detail);
+            session.getTransaction().commit();
+
+            System.out.println("Transaction was well done");
+        } finally {
+            session.close();
+            factory.close();
+        }
+    }
+}
--- src/main/java/kz/zhelezyaka/hebernateRelationsExample/entity/Detail.java
+++ src/main/java/kz/zhelezyaka/hebernateRelationsExample/entity/Detail.java
@@ -20,6 +20,9 @@ public class Detail {
     @Column(name = "email")
     private String email;
 
+    @OneToOne(mappedBy = "employeeDetail", cascade = CascadeType.ALL)
+    private Employee employee;
+
     public Detail() {
     }
 
@@ -59,6 +62,14 @@ public class Detail {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
     }
 
     @Override
