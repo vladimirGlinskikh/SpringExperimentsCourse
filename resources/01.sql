b5fab04273b1ca55a09782eabfe14c00ed438df6
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2022-04-21 12:02:33 +0600

    Relations One-To-Many (Bi-directional)

--- resources/01.sql
+++ resources/01.sql
@@ -3,6 +3,15 @@ CREATE DATABASE IF NOT EXISTS my_db;
 
 USE my_db;
 
+CREATE TABLE departments
+(
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(15),
+    max_salary INT,
+    min_salary INT,
+    PRIMARY KEY (id)
+);
+
 CREATE TABLE employees
 (
     id INT NOT NULL AUTO_INCREMENT,
@@ -14,26 +23,7 @@ CREATE TABLE employees
     FOREIGN KEY (department_id) REFERENCES departments(id)
 );
 
-CREATE TABLE details
-(
-    id int NOT NULL AUTO_INCREMENT,
-    city VARCHAR(15),
-    phone_number VARCHAR(25),
-    email VARCHAR(30),
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE departments
-(
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(15),
-    max_salary INT,
-    min_salary INT,
-    PRIMARY KEY (id)
-);
-
 SHOW TABLES;
 
-SELECT * FROM details;
 SELECT * FROM employees;
 SELECT * FROM departments;
---
+++ src/main/java/kz/zhelezyaka/hibernateOneToManyRelationsExample/Test1.java
@@ -0,0 +1,71 @@
+package kz.zhelezyaka.hibernateOneToManyRelationsExample;
+
+import kz.zhelezyaka.hibernateOneToManyRelationsExample.entity.Department;
+import kz.zhelezyaka.hibernateOneToManyRelationsExample.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Test1 {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .addAnnotatedClass(Department.class)
+                .buildSessionFactory();
+
+        Session session = null;
+        try {
+//            session = factory.getCurrentSession();
+//
+//            Department department1 = new Department("IT", 400, 1300);
+//            Department department2 = new Department("HR", 500, 1200);
+//            Employee employee1 = new Employee("Vladimir", "Glinskikh", 700);
+//            Employee employee2 = new Employee("Andrey", "Shatalov", 600);
+//            Employee employee3 = new Employee("Svetlana", "Svetikova", 800);
+//
+//            department1.addEmployeeToDepartment(employee1);
+//            department1.addEmployeeToDepartment(employee2);
+//            department2.addEmployeeToDepartment(employee3);
+//
+//            session.beginTransaction();
+//            session.save(department1);
+//            session.save(department2);
+//
+//            session.getTransaction().commit();
+//            System.out.println("Transaction was well done");
+
+//            session = factory.getCurrentSession();
+//
+//            session.beginTransaction();
+//            Department department = session.get(Department.class, 2);
+//            System.out.println(department);
+//            System.out.println(department.getEmployees());
+//
+//            session.getTransaction().commit();
+//            System.out.println("Transaction was well done");
+
+//            session = factory.getCurrentSession();
+//
+//            session.beginTransaction();
+//            Employee employee = session.get(Employee.class, 3);
+//            System.out.println(employee);
+//            System.out.println(employee.getDepartment());
+//
+//            session.getTransaction().commit();
+//            System.out.println("Transaction was well done");
+
+            session = factory.getCurrentSession();
+
+            session.beginTransaction();
+            Employee employee = session.get(Employee.class, 2);
+            session.delete(employee);
+
+            session.getTransaction().commit();
+            System.out.println("Transaction was well done");
+        } finally {
+            session.close();
+            factory.close();
+        }
+    }
+}
